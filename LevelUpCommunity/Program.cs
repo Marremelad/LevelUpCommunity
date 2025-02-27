b9acf69fd7421e4cb72869a596eaa37b736d60f7
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-27 23:56:04 +0100

    Removed comment

--- LevelUpCommunity/Program.cs
+++ LevelUpCommunity/Program.cs
@@ -8,8 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
+        
         builder.Services.AddAuthorization();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
